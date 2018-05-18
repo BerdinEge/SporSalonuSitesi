3c9a9c7e0ce59d29bc15460a4080a59e3c01228e
Berdin Ege <berdin_turgut@hotmail.com>
2018-05-19 01:23:56 +0300

    yükleme
    asd

---
+++ MacfitProjesi/MacfitProjesi/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MacfitProjesi.Models;
+
+namespace MacfitProjesi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Uyelikler()
+        {
+            ViewData["Message"] = "Üyelik sayfanız burası.";
+
+            return View();
+        }
+
+        public IActionResult Galeri()
+        {
+            ViewData["Message"] = "Galeri sayfanız.";
+
+            return View();
+        }
+
+        public IActionResult Kaydol()
+        {
+            return View();
+        }
+
+        public IActionResult Giris()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
