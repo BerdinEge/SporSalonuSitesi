370ecd1eeb2168632976f07a72b7f0068bd3928a
Berdin Ege <berdin_turgut@hotmail.com>
2018-05-20 19:34:47 +0300

    1.3.0

---
+++ MacfitProjesi/MacfitProjesi/Models/UyelerModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MacfitProjesi.Models
+{
+    public class UyelerModel
+    {
+    }
+}
