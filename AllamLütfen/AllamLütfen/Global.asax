5ccdf2914373ecd7a717462f1331d36682bfd84a
Doğa <d.ogut734@gmail.com>
2018-05-22 02:00:12 +0300

    allam

---
+++ AllamLütfen/AllamLütfen/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AllamLütfen.MvcApplication" Language="C#" %>
---
+++ AllamLütfen/AllamLütfen/Models/MyCoursesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AllamLütfen.Models
+{
+    public class MyCoursesViewModel
+    {
+        
+        public string Kurs_Icerik { get; set; }
+        public string Egitmen_Ad_Soyad { get; set; }
+        public string LSehir { get; set; }
+        public string LIlce { get; set; }
+        public string LAdres { get; set; }
+        public string LTel { get; set; }
+    }
+}
